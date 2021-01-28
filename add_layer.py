4afee8cf642dbe0f310a3acd09682f59f41591fd
qhjstc <654390596@qq.com>
2021-01-28 18:24:43 +0800

    build neural network , intermediate version

--- add_layer.py
+++ add_layer.py
@@ -1,11 +1,12 @@
 
 import tensorflow as tsf
+import numpy as np
 
 tsf.compat.v1.disable_eager_execution()       # in tensorflow 2.0 eager execution is enabled by default
 tf = tsf.compat.v1            # change the version
 
 
-def add_layer(inputs, in_size, out_size, activation_function = None):
+def add_layer(inputs, in_size, out_size, activation_function=None):
     weights = tf.Variable(tf.random_normal([in_size, out_size]))       # Create a random matrix
     biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)        # Generally, it is not set to 0, so we set it to 0.1
     wx_plus_b = tf.matmul(inputs, weights) + biases           # matrix multiplication, but i have some questions in here
@@ -15,3 +16,8 @@ def add_layer(inputs, in_size, out_size, activation_function = None):
         outputs = activation_function(wx_plus_b)             # Excitation equation
     return outputs
 
+
+if __name__ == "__main__":
+    x_data = np.linspace(-1, 1, 300)[:, np.newaxis]         # input only has one element , so it only has one neuron
+    noise = np.random.normal(0, 0.05, x_data.shape)
+    y_data = np.square(x_data) - 0.5 + noise
